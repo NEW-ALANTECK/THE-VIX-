02c5cef8c8007b8e422fd170ab6b0163ebf6e1d8
NEW-ALANTECK <88052550+NEW-ALANTECK@users.noreply.github.com>
2021-07-27 13:10:09 -0500

    Update dados.py

--- herramientas/dados.py
+++ herramientas/dados.py
@@ -3,7 +3,7 @@ Block="/etc/bin" && [[ ! -d ${Block} ]] && exit
 Block > /dev/null 2>&1
 clear
 cowsay -f eyes "esta herramienta ayuda aproteger tu vps de un ataque DDOS" | lolcat 
-figlet ..dankelthaher.. | lolcat
+figlet ..THE-VIX-PERÚ.. | lolcat
 BARRA1="\e[0;31m➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖\e[0m"
 echo -e "$BARRA1"
 
